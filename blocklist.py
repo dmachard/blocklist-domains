4bef49d82f226c17b274fecebb384d05fc33b7e2
dmachard <d.machard@gmail.com>
2020-10-31 14:48:01 +0100

    add hosts file support

--- blocklist.py
+++ blocklist.py
@@ -1,5 +1,8 @@
 
 import blocklist_aggregator
 
+# create raw blocklist
 blocklist_aggregator.save_raw(filename="blocklist.txt")
-     
+
+# create blocklist with hosts format
+blocklist_aggregator.save_hosts(filename="hosts.txt") 
