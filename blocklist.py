ec3ee1cf495ce7a3400b77603f38f55ed11835b5
dmachard <d.machard@gmail.com>
2020-10-31 17:11:40 +0100

    add whitelist

--- blocklist.py
+++ blocklist.py
@@ -1,8 +1,18 @@
 
 import blocklist_aggregator
 
+ext_cfg = """
+whitelist:
+  - amazonaws.com
+  - s3.amazonaws.com
+  - localhost.localdomain
+  - t.co
+"""
+
 # create raw blocklist
-blocklist_aggregator.save_raw(filename="blocklist.txt")
+blocklist_aggregator.save_raw(filename="blocklist.txt",
+                              ext_cfg=ext_cfg)
 
 # create blocklist with hosts format
-blocklist_aggregator.save_hosts(filename="hosts.txt") 
+blocklist_aggregator.save_hosts(filename="hosts.txt",
+                                ext_cfg=ext_cfg) 
