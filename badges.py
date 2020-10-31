e9478a6402ef3009a6d2675f02d2b9220fb7bee8
dmachard <d.machard@gmail.com>
2020-10-31 20:22:12 +0100

    use pybadges library instead of gh-actions

---
+++ badges.py
@@ -0,0 +1,22 @@
+from pybadges import badge
+from datetime import date
+
+# create badge pour the date
+b1 = badge(left_text='Last updated', 
+          right_text=date.today(),
+          right_color='blue')
+          
+with open("badge_date.svg", "w") as f:
+    f.write(b1)
+    
+# create badge pour the number of hosts
+with open("blocklists.txt, "r") as f:
+    data = f.read()
+domains = data.splitlines()
+
+b2 = badge(left_text='Aggregated hosts', 
+           right_text="%s" % len(domains),
+           right_color='red')
+           
+with open("badge_total.svg", "w") as f:
+    f.write(b2)
